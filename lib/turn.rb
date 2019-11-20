7c6422804d537ca47091ce9ce70ba79501722266
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-20 04:24:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,8 +30,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-      token = character(board)
-      move(board, index, token)
+      move(board, index, character(board))
       display_board(board)
   else
       turn(board)
